1e8ba537349443c7ec5ebf9b14edeb3ef606129a
hobutt <hobuttt.ne@gmail.ru>
2019-10-24 20:10:08 +0300

    configs

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      msg: 'Hi pall'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
