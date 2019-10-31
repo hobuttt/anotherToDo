5f0dd7f9758171f022db82972c9b89978c1b4a33
hobutt <Ghjujy27>
2019-10-31 08:18:12 +0300

    Create modal stsrt

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
+    <transition mode="out-in" name="fade">
 <!--    <img alt="Vue logo" src="./assets/logo.png">-->
     <TodoList/>
+  </transition>
     <div class="create-task">
-      <button class="create-task__btn">+</button>
+      <button class="create-task__btn" @click="createTask">+</button>
     </div>
+    <CreateTaskModal/>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList'
+import CreateTaskModal from './components/CreateTaskModal'
 
 export default {
   name: 'app',
-  components: {
-    TodoList
+  data: () => ({
+  }),
+  components: { TodoList, CreateTaskModal },
+  methods: {
+    createTask () {
+      this.$root.$emit('openCreateModal', true)
+    }
   }
 }
 </script>
@@ -33,10 +42,11 @@ export default {
     &__btn {
       font-size: 2rem;
       border-radius: 10px;
-      padding: 10px 15px;
-      padding-top: 5px;
+      padding: 8px 15px;
+      /*padding-top: 5px;*/
       outline: none;
-      transition: background .2s;
+      transition: box-shadow .2s;
+      background-color: #f6f6f6;
       &:hover {
         box-shadow: 1px 2px 5px 1px rgba(0, 0, 0, 0.3);
         transition: box-shadow .1s linear;
